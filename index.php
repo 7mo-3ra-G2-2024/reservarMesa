11868060d6a665a28e601e02d9b62996fa763c0a
Rusiton <galassosantiago05@gmail.com>
2024-04-15 21:11:34 -0300

    Se han modificado los archivos existentes

--- index.php
+++ index.php
@@ -4,49 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        th, td{
-            width: 5vw;
-            text-align: center;
-        }
-
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
+<header>
+    
+</header>
 <body>
     <center>
-        <h1>Turnos - Disponibilidad Horaria</h1>
+        <h1>Restaurante - Mapa de mesas</h1>
+        <div class="colorInfo">
+            <section><b><p style="color:green;">Verde:</p></b><p>Disponible</p></section>
+            <section><b><p style="color:red;">Rojo:</p></b><p>Reservado</p></section>
+        </div>
         <?php
-            $file = fopen("turnos.csv", "r");
+            $file = fopen("mesas.csv", "r");
             while(!feof($file)){
                 $data[] = explode("|", fgets($file));
             }
             fclose($file);
 
             $hourCount = 10;
-            $turnCount = 1;
+            $tableId = 1;
             $date = 1;
             $month = 4;
-            echo "<table border=1 style='min-height:30vh; width:60vw;'><tr><th>Horario</th>";
-            for($i = 0; $i < 6; $i++){
-                echo "<th>".$date."/".$month."</th>";
-                $date++;
-            }
+            echo "<table style='min-height:60vh; width:60vw;'><tr>";
+            echo "<th>Fila 1</th><th>Fila 2</th><th>Fila 3</th><th>Fila 4</th><th>Fila 5</th><th>Fila 6</th><th>Fila 7</th>";
             echo "</tr>";
             for($i = 0; $i < count($data); $i++){
-                echo "<tr>";
-                echo "<td>".$hourCount.":00</td>";
-                $hourCount = $hourCount + 2;
-                for($j = 0; $j < 6; $j++){
+                for($j = 0; $j < 7; $j++){
                     if($data[$i][$j] == "disponible"){
-                        echo "<td style='background-color:green;'><a href='pedir.php?num=".$turnCount."'>Solicitar</a></td>";
-                    }
-                    if($data[$i][$j] == "porasignar"){
-                        echo "<td style='background-color:orange'></td>";
+                        echo "<td><div class='mesa' style='background-color:green;'><a class='reserveButton' href='pedir.php?num=".$tableId."'>Reservar</a></div></td>";
                     }
                     if($data[$i][$j] == "nodisponible"){
-                        echo "<td style='background-color:red'></td>";
+                        echo "<td><div class='mesa' style='background-color:red'></div></td>";
                     }
-                    $turnCount++;
+                    $tableId++;
                 }
                 echo "</tr>";
             }
--- pedir.php
+++ pedir.php
@@ -4,44 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
     <style>
         form{
+            margin-top: 15vh;
             width: 20vw;
-            padding: 10vw;
+            height: 40vh;
+            padding: 5vw;
+            border: 1px;
+            border-radius: 3vw;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            background-color: cadetblue;
         }
         input, textarea, select{
-            width: 90%;
+            background-color: antiquewhite;
+            width: 15vw;
+            height: 3vw;
             margin-bottom: 2vh;
+        }
+        input[type="submit"]:hover{
+            cursor: grab;
+            transform: scale(1.1);
+            transition: 300ms;            
         }
     </style>
 </head>
+<header>
+    
+</header>
 <body>
     <center>
-        <?php
-            if(!isset($_GET["num"])){
-                header("Location: index.php");
-                die();
-            }
-            echo "<h2>Turno n°".$_GET["num"]."</h2>";
-        ?>
         <form action="pedir.php?">
+            <?php
+                if(!isset($_GET["num"])){
+                    header("Location: index.php");
+                    die();
+                }
+                echo "<h2>Mesa n°".$_GET["num"]."</h2>";
+            ?>
             <input type="hidden" name="num" value="<?php echo $_GET["num"] ?>">
             <input type="text" name="dni" placeholder="DNI" required>
             <input type="text" name="apellido" placeholder="Apellido" required>
             <input type="text" name="nombre" placeholder="Nombre" required>
-            <input type="text" name="direccion" placeholder="Dirección" required>
-            <textarea name="motivo" placeholder="Motivo de la consulta" required></textarea>
             <select name="metodo">
                 <option name="efectivo">Efectivo</option>
                 <option name="tarjeta">Tarjeta</option>
             </select>
-            <input type="submit" name="submitted" value="Pedir turno">
+            <input type="submit" name="submitted" value="Reservar mesa">
         </form>
     </center>
     <?php
         if(isset($_GET["submitted"])){
             $file = fopen("pendientes.csv", "a");
-            fwrite($file, "\n".$_GET["dni"]."|".$_GET["apellido"]."|".$_GET["nombre"]."|".$_GET["direccion"]."|".$_GET["motivo"]."|".$_GET["metodo"]);
+            fwrite($file, "\n".$_GET["dni"]."|".$_GET["apellido"]."|".$_GET["nombre"]."|".$_GET["metodo"]);
             fclose($file);
             header("Location: index.php");
         }
